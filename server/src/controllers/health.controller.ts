53997912f663a78600e501600330f18533239c37
Etienne Beaulac <etienneb@zillowgroup.com>
2025-06-19 02:18:56 -0600

    remove explicit logging

--- server/src/controllers/health.controller.ts
+++ server/src/controllers/health.controller.ts
@@ -1,4 +1,5 @@
 // server/src/controllers/health.controller.ts
+
 import { Request, Response } from 'express';
 import { checkHealth } from '../services/health.service';
 
@@ -9,13 +10,9 @@ import { checkHealth } from '../services/health.service';
 export const getHealthStatus = async (req: Request, res: Response) => {
     const healthStatus = await checkHealth();
 
-    // The controller is responsible for request-specific logging.
     if (healthStatus.status === 'OK') {
-        req.log.info('Health check successful');
         res.status(200).json(healthStatus);
     } else {
-        // Here, we log the error in the context of a failed request.
-        req.log.error('Health check failed: Database connection is unavailable.');
         res.status(503).json(healthStatus);
     }
 };
